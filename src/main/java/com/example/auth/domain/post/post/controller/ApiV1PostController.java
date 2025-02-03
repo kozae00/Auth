5a91c8cbbccfff9ad15456dc73295a82b3eb7a29
kozae00 <rhwogus777@gmail.com>
2025-02-03 14:22:40 +0900

    password2 apply

--- src/main/java/com/example/auth/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/auth/domain/post/post/controller/ApiV1PostController.java
@@ -10,7 +10,6 @@ import com.example.auth.global.exception.ServiceException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.web.bind.annotation.*;
@@ -131,7 +130,7 @@ public class ApiV1PostController {
 
         Member actor = memberService.findById(authorId).get();
 
-        if (!actor.getPassword().equals(password)) {
+        if (!actor.getPassword2().equals(password)) {
             throw new ServiceException("401-1", "비밀번호가 일치하지 않습니다.");
         }
 
