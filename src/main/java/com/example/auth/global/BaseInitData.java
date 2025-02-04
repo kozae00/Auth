b1229795f92ae04cf0106d2910af2cf677a19889
kozae00 <rhwogus777@gmail.com>
2025-02-04 09:33:35 +0900

    comment init data

--- src/main/java/com/example/auth/global/BaseInitData.java
+++ src/main/java/com/example/auth/global/BaseInitData.java
@@ -2,6 +2,7 @@ package com.example.auth.global;
 
 import com.example.auth.domain.member.member.entity.Member;
 import com.example.auth.domain.member.member.service.MemberService;
+import com.example.auth.domain.post.post.entity.Post;
 import com.example.auth.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -57,8 +58,13 @@ public class BaseInitData {
         Member user2 = memberService.findByUsername("user2").get();
 
 
-        postService.write(user1, "title1", "content1");
-        postService.write(user1, "title2", "content2");
+        Post post1 = postService.write(user1, "해외축구에서 누가 축구 제일 잘하는 것 같아?", "킹찍히 호날두가 goat인듯");
+        post1.addComment(user1, "젖닌이 수듄");
+        post1.addComment(user2, "메시가 GOAT이지...");
+
+        Post post2 = postService.write(user1, "배가 고픈데", "피자를 먹을까? 치킨을 먹을까?");
+        post2.addComment(user1, "족발 먹으렴.");
+
         postService.write(user2, "title3", "content3");
 
     }
