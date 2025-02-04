7466293917fff7861839be894b4b4a502b66c567
kozae00 <rhwogus777@gmail.com>
2025-02-04 10:11:55 +0900

    get comment

--- src/main/java/com/example/auth/domain/post/comment/controller/ApiV1CommentController.java
+++ src/main/java/com/example/auth/domain/post/comment/controller/ApiV1CommentController.java
@@ -1,8 +1,17 @@
 package com.example.auth.domain.post.comment.controller;
 
+import com.example.auth.domain.post.comment.dto.CommentDto;
+import com.example.auth.domain.post.comment.entity.Comment;
+import com.example.auth.domain.post.post.entity.Post;
+import com.example.auth.domain.post.post.service.PostService;
+import com.example.auth.global.exception.ServiceException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -10,5 +19,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ApiV1CommentController {
 
+    private final PostService postService;
+
+    @GetMapping
+    public List<CommentDto> getItems(@PathVariable long postId) {
+        Post post = postService.getItem(postId).orElseThrow(
+                () -> new ServiceException("404-1", "존재하지 않는 게시글입니다.")
+        );
+        return post.getComments()
+                .stream()
+                .map(CommentDto::new)
+                .toList();
+    }
+
+    @GetMapping("{id}")
+    public CommentDto getItem(@PathVariable long postId, @PathVariable long id) {
+        Post post = postService.getItem(postId).orElseThrow(
+                () -> new ServiceException("404-1", "존재하지 않는 게시글입니다.")
+        );
+
+        Comment comment = post.getCommentById(id);
+
+        return new CommentDto(comment);
+    }
 
 }
