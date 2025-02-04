59743b31b4b939c3d401fa2c53fd56ce8c49c9c4
kozae00 <rhwogus777@gmail.com>
2025-02-04 09:15:32 +0900

    comment domain add

---
+++ src/main/java/com/example/auth/domain/post/comment/controller/ApiV1CommentController.java
@@ -0,0 +1,14 @@
+package com.example.auth.domain.post.comment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/comments")
+@RequiredArgsConstructor
+public class ApiV1CommentController {
+
+
+}
